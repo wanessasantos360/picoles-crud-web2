a5d96816d160118c50112cb5dafe2a39e03b0aa0
Wanessa Santos <wanessa.ufpe2018@gmail.com>
2025-02-16 21:24:47 -0300

    Configurações do JWT

--- core/settings.py
+++ core/settings.py
@@ -1,3 +1,8 @@
 from sqlalchemy.orm import declarative_base
 
 ModelBase = declarative_base()
+
+JWT_SECRET: str = 'AMWv2ZK68wdca61mlH1willKQq-1TBakT36goW34XNc'
+ALGORITHM: str = 'HS256'
+EXPIRATION_MINUTES: int = 60 * 24 * 7
+DATE_FORMAT = '%d/%m/%Y'
